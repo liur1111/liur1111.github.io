d236e3fc1a9474988bd7c1c634715ec723b238b5
liur1111 <liur@mit.edu>
2023-09-27 18:43:08 -0400

    GCN Launcher and Portfolio

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,107 +1,15 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import ImageGallery from "react-image-gallery";
+import { useRef } from "react";
 import Layout from "../components/layout.js";
 import ProjectContext from "../store.js";
-import personal_portfolio from "../images/projects/personal-portfolio/cover-photo.png";
-import roach_radio from "../images/projects/roach-radio/cover-photo.png";
-import gcn_launcher from "../images/projects/gcn-launcher/cover-photo.png";
-import music_now from "../images/projects/music-now/cover-photo.png";
-import mogi from "../images/projects/mogi/cover-photo.png";
-import keywords_clustering_tool from "../images/projects/keywords-clustering-tool/cover-photo.png";
-import nice_rack from "../images/projects/nice-rack/cover-photo.png";
-import roachradiomotto from "../images/projects/RoachRadioMotto.png";
-import roachradiomotto2 from "../images/projects/RoachRadioMotto2.png";
-import letscelebrateballs from "../images/projects/letscelebrateballs.png";
-import shoomverns from "../images/projects/Shoomverns.jpeg";
-import sesfverns from "../images/projects/SESFverns.png";
-import begoosedocrime from "../images/projects/BeGooseDoCrime.png";
+import PROJECTS from "../components/project_list.js";
 
 import "./projects.css";
 import "../components/project_layout.css";
 
 const FILTERS = ["all", "personal", "work", "design"];
-
-const PROJECTS = [
-  {
-    title: "GCN Launcher",
-    category: "work",
-    path: "/projects/gcn-launcher",
-    cover: gcn_launcher,
-    orientation: "vertical",
-  },
-  {
-    title: "Roach Radio",
-    category: "personal",
-    path: "/projects/roach-radio",
-    cover: roach_radio,
-    orientation: "horizontal",
-  },
-  {
-    title: "Personal Portfolio",
-    category: "personal",
-    path: "/projects/personal-portfolio",
-    cover: personal_portfolio,
-    orientation: "horizontal",
-  },
-  {
-    title: "Music Now",
-    category: "personal",
-    path: "/projects/music-now",
-    cover: music_now,
-    orientation: "horizontal",
-  },
-  {
-    title: "Mogi",
-    category: "personal",
-    path: "/projects/mogi",
-    cover: mogi,
-    orientation: "horizontal",
-  },
-  {
-    title: "Keywords Clustering Tool",
-    category: "work",
-    path: "/projects/keywords-clustering-tool",
-    cover: keywords_clustering_tool,
-    orientation: "horizontal",
-  },
-  {
-    title: "Nice Rack",
-    category: "personal",
-    path: "/projects/nice-rack",
-    cover: nice_rack,
-    orientation: "none",
-  },
-  {
-    category: "design",
-    cover: letscelebrateballs,
-    orientation: "none",
-  },
-  {
-    category: "design",
-    cover: begoosedocrime,
-    orientation: "none",
-  },
-  {
-    category: "design",
-    cover: shoomverns,
-    orientation: "vertical",
-  },
-  {
-    category: "design",
-    cover: roachradiomotto,
-    orientation: "horizontal",
-  },
-  {
-    category: "design",
-    cover: sesfverns,
-    orientation: "none",
-  },
-  {
-    category: "design",
-    cover: roachradiomotto2,
-    orientation: "horizontal",
-  },
-];
 
 const ProjectsPage = () => {
   const { filter, setFilter } = React.useContext(ProjectContext);
@@ -134,11 +42,11 @@ const ProjectsPage = () => {
               : project["category"] !== "design"
           ).map((project, index) => {
             return (
-              <div class={"image-container " + project["orientation"]}>
+              <div class={"image-container " + project["cover"]["orientation"]}>
                 {project["category"] !== "design" ? (
                   <Link to={project["path"]}>
                     <img
-                      src={project["cover"]}
+                      src={project["cover"]["src"]}
                       alt={project["title"]}
                       class="project-image"
                     />
@@ -148,10 +56,10 @@ const ProjectsPage = () => {
                   </Link>
                 ) : (
                   <img
-                    src={project["cover"]}
+                    src={project["cover"]["src"]}
                     alt=""
                     class="project-image"
-                    onClick={() => setShowFullScreen(project["cover"])}
+                    onClick={() => setShowFullScreen(project["_id"])}
                   />
                 )}
               </div>
@@ -161,8 +69,14 @@ const ProjectsPage = () => {
       </div>
       {showFullScreen !== "" && (
         <div class="fullscreen-overlay" onClick={() => setShowFullScreen("")}>
-          <div class="fullscreen-image">
-            <img src={showFullScreen} alt="" />
+          <div
+            class={
+              "fullscreen-image " +
+              PROJECTS[showFullScreen]["cover"]["orientation"] +
+              "-fullscreen"
+            }
+          >
+            <img src={PROJECTS[showFullScreen]["cover"]["src"]} alt="" />
           </div>
         </div>
       )}
--- src/pages/projects/gcn-launcher.js
+++ src/pages/projects/gcn-launcher.js
@@ -1,77 +1,87 @@
 import * as React from "react";
 import ProjectLayout from "../../components/project_layout.js";
+import PROJECTS from "../../components/project_list.js";
 import "../../components/project_layout.css";
-import coverPhoto from "../../images/projects/gcn-launcher/cover-photo.png";
 
 const GCNLauncher = () => {
   const [showFullScreen, setShowFullScreen] = React.useState("");
+  const project = PROJECTS[0];
   return (
     <ProjectLayout projectTitle="GCN Launcher">
       <div class="new-stack">
         <img
-          src={coverPhoto}
+          src={project["photo1"]["src"]}
           alt=""
-          class="project-media"
-          onClick={() => setShowFullScreen(coverPhoto)}
+          class="project-media screenshot"
+          onClick={() => setShowFullScreen("photo1")}
         />
         <div class="project-description">
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
+          <div>
+            <span
+              class="external-links"
+              onClick={() => window.open("https://www.graal.cloud/gcn")}
+            >
+              Graal Cloud Native (GCN)
+            </span>{" "}
+            is an Oracle build of the open source Micronaut framework. GCN
+            provides a curated set of Micronaut framework modules that simplify
+            cloud application development, are designed to be compiled
+            ahead-of-time with GraalVM Native Image, and are fully supported by
+            Oracle. GCN enables you to easily build portable cloud native Java
+            microservices that start instantly and use fewer resources to reduce
+            compute costs.
+          </div>
         </div>
       </div>
       <div class="new-stack">
         <div class="project-description text-right">
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
+          <div>
+            I have designed and built a majority of the features of the{" "}
+            <span
+              class="external-links"
+              onClick={() =>
+                window.open("https://www.graal.cloud/gcn/launcher")
+              }
+            >
+              GCN launcher
+            </span>
+            , which facilitates the process of creating a GCN app. The more
+            noticeable features include previewing the entire code directory of
+            an app, and displaying differences in code between 2 GCN apps.
+          </div>
         </div>
         <img
-          src={coverPhoto}
+          src={project["photo2"]["src"]}
           alt=""
-          class="project-media"
-          onClick={() => setShowFullScreen(coverPhoto)}
+          class="project-media screenshot"
+          onClick={() => setShowFullScreen("photo2")}
         />
       </div>
       <div class="new-stack">
         <img
-          src={coverPhoto}
+          src={project["photo3"]["src"]}
           alt=""
-          class="project-media"
-          onClick={() => setShowFullScreen(coverPhoto)}
+          class="project-media screenshot"
+          onClick={() => setShowFullScreen("photo3")}
         />
-        <div class="project-description">
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-        </div>
+        <iframe
+          class="project-media figma"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FnCeE1qcu0K80qcq3aAzYwd%2FUntitled%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DRQpcPGLKRR0R2Qnu-1"
+          allowfullscreen="true"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+        ></iframe>
       </div>
-      {/* <div class="new-stack">
-        <div class="project-description text-right">
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-          description description descriptiondescrip description description
-        </div>
-        <video muted controls class="project-media">
-          <source src={video1} type="video/mp4" />
-        </video>
-      </div> */}
       {showFullScreen !== "" && (
         <div class="fullscreen-overlay" onClick={() => setShowFullScreen("")}>
-          <div class="fullscreen-image">
-            <img src={showFullScreen} alt="" />
+          <div
+            class={
+              "fullscreen-image " +
+              project[showFullScreen]["orientation"] +
+              "-fullscreen"
+            }
+          >
+            <img src={project[showFullScreen]["src"]} alt="" />
           </div>
         </div>
       )}
