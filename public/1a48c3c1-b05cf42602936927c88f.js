492206b8aa12c0615525d56c8bba0b2ad68a40f1
liur1111 <liur@mit.edu>
2023-09-27 23:38:22 -0400

    new site

---
+++ public/1a48c3c1-b05cf42602936927c88f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrachel_liu=self.webpackChunkrachel_liu||[]).push([[874],{7516:function(t,u,a){a.d(u,{YiX:function(){return n}});var e=a(4405);function n(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"}}]})(t)}}}]);
+//# sourceMappingURL=1a48c3c1-b05cf42602936927c88f.js.map
