8173bee37afc8a4663db1239fbced9383e254b5c
Rachel Liu <rachelliu@alum.mit.edu>
2024-05-23 22:38:47 -0400

    First Commit

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ProjectProvider } from "./src/store.js";
-
-export const wrapRootElement = ({ element }) => (
-  <ProjectProvider>{element}</ProjectProvider>
-);
